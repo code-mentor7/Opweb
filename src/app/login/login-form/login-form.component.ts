982f926d69f58077816d8f5ffc165949f8f49560
jinjinsu <jjs19930421@gmail.com>
2019-02-03 23:25:24 +0800

    add login logic

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -7,7 +7,32 @@ import { AuthData } from '../../auth-data';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  email = '';
+  password = '';
+  inputFocused = new EventEmitter<boolean>();
+  requiredFields = {
+    email: false,
+    password: false
+  };
+  @Output() login = new EventEmitter<AuthData>();
+  @Input() loginFail: boolean;
 
   constructor() { }
+
+  onSubmit() {
+    if (!this.email || !this.password) {
+      if (!this.email) {
+        this.requiredFields.email = true;
+      }
+      if (!this.password) {
+        this.requiredFields.password = true;
+      }
+    } else {
+      this.login.emit({email: this.email, password: this.password});
+    }
+  }
+
+  ngOnInit() {
+  }
 
 }
