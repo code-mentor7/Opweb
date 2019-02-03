4c94f233693e93f72e3746b973b59b5524c46c70
jinjinsu <jjs19930421@gmail.com>
2019-02-03 23:19:12 +0800

    add logic form - Frontend

---
+++ src/app/login/login-form/login-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { AuthData } from '../../auth-data';
+
+@Component({
+  selector: 'login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+  constructor() { }
+
+}
